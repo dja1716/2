cd29e3a7472a93cbc0d554e99b02f74b7b313c1d
dja1716 <hdum@uwaterloo.ca>
2021-11-09 21:01:02 +0900

    Complete

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -79,7 +79,7 @@ export default function App() {
               duration: 100,
               easing: Easing.linear,
             }),
-          ]).start();
+          ]).start(nextIcon);
         } else {
           Animated.parallel([onPressOut, goHome]).start();
         }
@@ -97,6 +97,14 @@ export default function App() {
     })
   ).current;
   // state
+  const [index, setIndex] = useState(0);
+  const nextIcon = () => {
+    setIndex((prev) => prev + 1);
+    Animated.parallel([
+      Animated.spring(scale, { toValue: 1, useNativeDriver: true }),
+      Animated.spring(opacity, { toValue: 1, useNativeDriver: true }),
+    ]).start();
+  };
   return (
     <Container>
       <Edge>
@@ -112,7 +120,7 @@ export default function App() {
             transform: [...position.getTranslateTransform(), { scale }],
           }}
         >
-          <Ionicons name="beer" color={GREY} size={76} />
+          <Ionicons name={icons[index]} color={GREY} size={76} />
         </IconCard>
       </Center>
       <Edge>
